3a5b6f5e61b840ed8c270221062a5ea4298108a0
pontep <pontep.taveeshup@gmail.com>
2020-08-24 13:02:48 +0700

    ok

--- springboot/src/main/java/com/example/uploadingfiles/controller/MainController.java
+++ springboot/src/main/java/com/example/uploadingfiles/controller/MainController.java
@@ -1,2 +1,13 @@
-package com.example.uploadingfiles.controller;public class MainController {
+package com.example.uploadingfiles.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/din")
+    public String testDin(){
+        return "Pontep Thaweesup";
+    }
 }
